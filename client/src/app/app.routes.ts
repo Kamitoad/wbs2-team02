db4a8dca873ce1dd3e7c19bdc42201c348a92bb0
Dimitrios Paliouras <dimitrios.paliouras@mni.thm.de>
2024-08-03 15:33:39 +0200

    In the styles.css the standard background color and text color are defined as a basis, as defined in the mock-up. HTML framework for the game frontend. Route for /game created.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {GameComponent} from "./pages/user/components/game/game.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: 'game', component: GameComponent },
+];
