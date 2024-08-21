6976ac7d6ee68ab110f436c332e48a1067e82980
Chasan Moustafa <chasan.moustafa@mni.thm.de>
2024-08-21 23:50:38 +0200

    Add: CreateUserDto.ts

---
+++ server/src/common/dtos/CreateUserDto.ts
@@ -0,0 +1,49 @@
+import {ApiProperty} from "@nestjs/swagger";
+import {IsBoolean, IsEmail, IsNotEmpty, IsString} from "class-validator";
+
+export class CreateUserDto {
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    userName: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    @IsEmail()
+    emailConfirm: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    passwordConfirm: string;
+
+    @ApiProperty()
+    @IsBoolean()
+    agb: boolean;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    firstName: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    lastName: string;
+
+    @ApiProperty()
+    @IsString()
+    profilePic: string;
+}
