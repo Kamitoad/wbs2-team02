a1b3ad40924e84f7849005212655c00a48799c98
Chasan Moustafa <chasan.moustafa@mni.thm.de>
2024-08-22 16:46:05 +0200

    Add: auth.controller.ts logic

--- server/src/common/controllers/auth/auth.controller.ts
+++ server/src/common/controllers/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Session,
+    UnauthorizedException } from '@nestjs/common';
+import { SessionData } from "express-session";
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    @Post('login')
+    login(
+        @Session() session: SessionData,
+        @Body('username') username: string,
+    ): void {
+        if (username == 'admin') {
+            session.isLoggedIn = true;
+        } else {
+            throw new UnauthorizedException();
+        }
+    }
+    @Post('logout')
+    logout(@Session() session: SessionData): void {
+        session.isLoggedIn = undefined;
+    }
+}
