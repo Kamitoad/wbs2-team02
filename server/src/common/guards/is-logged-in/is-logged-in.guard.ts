c3e6c4db36e529932a1eb1f861be697868a68bf9
Chasan Moustafa <chasan.moustafa@mni.thm.de>
2024-08-22 16:48:51 +0200

    Add: is-logged-in.guard + logic

---
+++ server/src/common/guards/is-logged-in/is-logged-in.guard.ts
@@ -0,0 +1,13 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Request } from 'express';
+import { SessionData } from "express-session";
+
+@Injectable()
+export class IsLoggedInGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const http = context.switchToHttp();
+    const request = http.getRequest<Request>();
+    const session: SessionData = request.session;
+    return session.isLoggedIn;  //returns true if user isLoggedIn
+  }
+}
