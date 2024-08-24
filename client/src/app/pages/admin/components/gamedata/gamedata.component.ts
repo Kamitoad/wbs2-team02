a598302dabbf1360c97933238611bce53b74a588
Chasan Moustafa <chasan.moustafa@mni.thm.de>
2024-08-24 23:00:43 +0200

    Add: gamedata.component + current-games.component + current-games-card.component

---
+++ client/src/app/pages/admin/components/gamedata/gamedata.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gamedata',
+  standalone: true,
+  imports: [],
+  templateUrl: './gamedata.component.html',
+  styleUrl: './gamedata.component.css'
+})
+export class GamedataComponent {
+
+}
