e422de239b7603b429b083d72ecc47fbc946a4c2
Chasan Moustafa <chasan.moustafa@mni.thm.de>
2024-08-24 23:27:09 +0200

    Fix: Route admin/game now directs to GamedataComponent

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,13 +3,14 @@ import {LoginComponent} from "./pages/user/components/login/login.component";
 import {RegisterComponent} from "./pages/user/components/register/register.component";
 import {UserdataComponent} from "./pages/admin/components/userdata/userdata.component";
 import {CurrentGamesComponent} from "./pages/admin/components/gamedata/current-games/current-games.component";
+import {GamedataComponent} from "./pages/admin/components/gamedata/gamedata.component";
 
 export const routes: Routes = [
 
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'admin/user', component: UserdataComponent },
-  { path: 'admin/game', component: UserdataComponent,
+  { path: 'admin/game', component: GamedataComponent,
     children: [
       {
         path: 'currentgames', component: CurrentGamesComponent,
