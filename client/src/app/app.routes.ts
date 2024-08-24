c051103f3b5e709dc75fef05d507d77541469ac6
Chasan Moustafa <chasan.moustafa@mni.thm.de>
2024-08-24 17:10:22 +0200

    Add: userdata.component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./pages/user/components/login/login.component";
 import {RegisterComponent} from "./pages/user/components/register/register.component";
+import {UserdataComponent} from "./pages/admin/components/userdata/userdata.component";
 
 export const routes: Routes = [
 
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
+  { path: 'admin/user', component: UserdataComponent},
 
 ];
