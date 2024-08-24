86a3eff5295f8ada65e875834e8f12832b71bdb1
Chasan Moustafa <chasan.moustafa@mni.thm.de>
2024-08-25 00:43:49 +0200

    Feat: Current games HTML done (!not responsive!)

--- client/src/app/pages/admin/components/gamedata/current-games/current-games.component.ts
+++ client/src/app/pages/admin/components/gamedata/current-games/current-games.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {CurrentGamesCardComponent} from "./current-games-card/current-games-card.component";
 
 @Component({
   selector: 'app-current-games',
   standalone: true,
-  imports: [],
+  imports: [
+    CurrentGamesCardComponent
+  ],
   templateUrl: './current-games.component.html',
   styleUrl: './current-games.component.css'
 })
