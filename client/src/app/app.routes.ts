c58d6307fcdf375e3989e7b54f6dfc3756c528e7
Chasan Moustafa <chasan.moustafa@mni.thm.de>
2024-08-24 23:24:47 +0200

    Fix: Put userdata-card.component.css classes in styles.css Add: Child route for current-games.component "admin/game/currentgames"

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,11 +2,19 @@ import { Routes } from '@angular/router';
 import {LoginComponent} from "./pages/user/components/login/login.component";
 import {RegisterComponent} from "./pages/user/components/register/register.component";
 import {UserdataComponent} from "./pages/admin/components/userdata/userdata.component";
+import {CurrentGamesComponent} from "./pages/admin/components/gamedata/current-games/current-games.component";
 
 export const routes: Routes = [
 
-  { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent},
-  { path: 'admin/user', component: UserdataComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'admin/user', component: UserdataComponent },
+  { path: 'admin/game', component: UserdataComponent,
+    children: [
+      {
+        path: 'currentgames', component: CurrentGamesComponent,
+      }
+    ]
+  },
 
 ];
--- client/src/app/pages/admin/components/gamedata/gamedata.component.ts
+++ client/src/app/pages/admin/components/gamedata/gamedata.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {RouterLink, RouterOutlet} from "@angular/router";
+import {UserdataCardComponent} from "../userdata/userdata-card/userdata-card.component";
 
 @Component({
   selector: 'app-gamedata',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    RouterLink,
+    UserdataCardComponent,
+    RouterOutlet
+  ],
   templateUrl: './gamedata.component.html',
   styleUrl: './gamedata.component.css'
 })
