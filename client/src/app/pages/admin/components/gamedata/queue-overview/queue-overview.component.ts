0e3b9a57a1b2ba0a1060b3a1815db806e23f1d77
Chasan Moustafa <chasan.moustafa@mni.thm.de>
2024-08-25 01:04:01 +0200

    Feat: Queue-Overview HTML/CSS (!not responsive!)

--- client/src/app/pages/admin/components/gamedata/queue-overview/queue-overview.component.ts
+++ client/src/app/pages/admin/components/gamedata/queue-overview/queue-overview.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {CurrentGamesCardComponent} from "../current-games/current-games-card/current-games-card.component";
+import {QueueOverviewCardComponent} from "./queue-overview-card/queue-overview-card.component";
 
 @Component({
   selector: 'app-queue-overview',
   standalone: true,
-  imports: [],
+  imports: [
+    CurrentGamesCardComponent,
+    QueueOverviewCardComponent
+  ],
   templateUrl: './queue-overview.component.html',
   styleUrl: './queue-overview.component.css'
 })
