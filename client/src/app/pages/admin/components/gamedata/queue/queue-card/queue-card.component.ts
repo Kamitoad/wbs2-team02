c50eb7edfd634f20e5790b11d1b733e9ee3c9548
Chasan Moustafa <chasan.moustafa@mni.thm.de>
2024-08-25 00:45:40 +0200

    Add: queue-card.component + queue.component

---
+++ client/src/app/pages/admin/components/gamedata/queue/queue-card/queue-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-queue-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './queue-card.component.html',
+  styleUrl: './queue-card.component.css'
+})
+export class QueueCardComponent {
+
+}
