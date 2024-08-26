20effe627598801b9fe25d4dc28b9778d22abd16
Chasan Moustafa <chasan.moustafa@mni.thm.de>
2024-08-26 15:02:46 +0200

    Add: ReadCurrentGamesDto.ts

--- server/src/modules/admin/dtos/ReadCurrentGamesDto.ts
+++ server/src/modules/admin/dtos/ReadCurrentGamesDto.ts
@@ -11,17 +11,25 @@ export class ReadCurrentGamesDto {
     @ApiProperty({example: 2006})
     player1Elo: number;
 
+    @ApiProperty( {example: "maxPb.png"})
+    player1ProfilePic: string;
+
     @ApiProperty({example: "Kamitoad"})
     player2UserName: string;
 
     @ApiProperty({example: 2002})
     player2Elo: number;
 
+    @ApiProperty( {example: "kamiPb.png"})
+    player2ProfilePic: string;
+
     constructor(game: Game) {
         this.gameId = game.gameId;
         this.player1UserName = game.player1.userName
         this.player1Elo = game.player1.elo
+        this.player1ProfilePic = game.player1.profilePic;
         this.player2UserName = game.player2.userName
         this.player2Elo = game.player2.elo
+        this.player2ProfilePic = game.player2.profilePic;
     }
 }
