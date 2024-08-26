0dbf9ba239b2981275876cd501213cbb893cbf12
Chasan Moustafa <chasan.moustafa@mni.thm.de>
2024-08-26 15:01:31 +0200

    Feat: current-games.components are now dynamic

--- client/src/app/pages/admin/components/gamedata/current-games/current-games-card/current-games-card.component.ts
+++ client/src/app/pages/admin/components/gamedata/current-games/current-games-card/current-games-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-current-games-card',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './current-games-card.component.css'
 })
 export class CurrentGamesCardComponent {
-
+  @Input() currentGame: any;
 }
--- client/src/app/pages/admin/components/gamedata/current-games/current-games.component.ts
+++ client/src/app/pages/admin/components/gamedata/current-games/current-games.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {CurrentGamesCardComponent} from "./current-games-card/current-games-card.component";
+import {GamedataService} from "../../../services/gamedata.service";
 
 @Component({
   selector: 'app-current-games',
@@ -11,5 +12,13 @@ import {CurrentGamesCardComponent} from "./current-games-card/current-games-card
   styleUrl: './current-games.component.css'
 })
 export class CurrentGamesComponent {
+  currentGames: any = [];
 
+  constructor(private gamedataService: GamedataService) {}
+
+  ngOnInit(): void {
+    this.gamedataService.currentGames$.subscribe((currentGames: any[]) => {
+      this.currentGames = currentGames;
+    });
+  }
 }
