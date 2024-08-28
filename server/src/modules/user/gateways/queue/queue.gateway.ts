54f43ec889ced4128db0b14d36c4d4943667c515
Chasan Moustafa <chasan.moustafa@mni.thm.de>
2024-08-29 00:04:12 +0200

    Add: queue.gateway.ts

---
+++ server/src/modules/user/gateways/queue/queue.gateway.ts
@@ -0,0 +1,20 @@
+import {SubscribeMessage, WebSocketGateway, WebSocketServer} from '@nestjs/websockets';
+import {Server, Socket} from "socket.io";
+
+@WebSocketGateway()
+export class QueueGateway {
+
+  @WebSocketServer()
+  server: Server;
+
+
+
+  @SubscribeMessage('joinQueue')
+  handleJoinQueue(client: Socket, payload: { userId: number }): void {
+    console.log(`User ${payload.userId} trat der Queue bei`);
+  }
+
+  sendQueueTime(userId: number, time: number): void {
+    this.server.emit('queueTimeUpdate', { userId, time });
+  }
+}
